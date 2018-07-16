a2fb569d492d3183b8901649017639b978e93311
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2018-07-15 23:06:55 -0300

    given an angle and orbit, find the position of a planet

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-	// Create a Paper.js Path to draw a line into it:
-	var path = new Path();
-	// Give the stroke a color
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	// Move to start and draw a line from there
-	path.moveTo(start);
-	// Note the plus operator on Point objects.
-	// PaperScript does that for us, and much more!
-	path.lineTo(start + [ 100, -50 ]);
+
+// paper.project.importSVG('luzeira.svg');
+
+// 0 = sun
+// 1 = moon
+// 2 = mercury
+// 3 = venus
+// 4 = mars
+// 5 = jupiter
+// 6 = saturn
+
+
+var radius = [
+  [251, 122],
+  [339, 165],
+  [426, 207],
+  [514, 250],
+  [601, 292],
+  [689, 335],
+  [777, 378],
+];
+
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+function planet_position(angle, radius) {
+  var x, y;
+  var a = radius[0]/2, b = radius[1]/2;
+
+  var theta = Math.radians(angle);
+
+  x = a * Math.cos(theta);
+  y = b * Math.sin(theta);
+
+  return new Point(x,y);
+}
+
+
+
+var path = new Path.Circle(new Point(), 10);
+path.fillColor = '#007cff';
+
+var planet = new Symbol(path);
+
+planet.place(new Point(400, 200));
+
+
+console.log(planet_position(45, radius[6]));
+
+
+
+// project.importSVG('astrologia.svg');
+
+
